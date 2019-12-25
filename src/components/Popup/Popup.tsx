f4f32666135211fd1d4857cb6bc20bf64ce54763
reginPekin <reginbegin@gmail.com>
2019-12-25 23:44:46 +0300

    Popup component

---
+++ src/components/Popup/Popup.tsx
@@ -0,0 +1,33 @@
+import React, { useRef, FunctionComponent } from "react";
+
+import cx from "classnames";
+
+import { useOnClickOutside } from "../../utils/hooks";
+
+import styles from "./Popup.module.css";
+
+interface Props {
+  isVisible: boolean;
+  changeVisibility(): any;
+  positionClassName?: string;
+}
+
+export const Popup: FunctionComponent<Props> = ({
+  isVisible,
+  children,
+  changeVisibility = () => null,
+  positionClassName = undefined
+}) => {
+  const divRef = useRef(null);
+  useOnClickOutside(divRef, () => changeVisibility());
+  return (
+    <div>
+      {isVisible && <div className={styles.glass} />}
+      {isVisible && (
+        <div ref={divRef} className={cx(styles.contextMenu, positionClassName)}>
+          {children}
+        </div>
+      )}
+    </div>
+  );
+};
