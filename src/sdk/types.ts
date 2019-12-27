db16fdab5d3946538ecc6586e18aae374b32eb08
reginPekin <reginbegin@gmail.com>
2019-12-27 23:29:14 +0300

    object types

---
+++ src/sdk/types.ts
@@ -0,0 +1,18 @@
+export interface Folder {
+  words: Word[];
+  name: string;
+  id: string;
+  foreignLanguage: string;
+  nativeLanguage: string;
+  sortMethod: SortMethod;
+}
+
+export interface Word {
+  foreignWord: string;
+  nativeWord: string;
+  wordId: string;
+  date: number;
+  speechPart: string;
+}
+
+export type SortMethod = "date" | "foreign" | "native" | "speech";
