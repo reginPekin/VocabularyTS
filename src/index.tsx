2ae7e0b6b59e9a5086f900e530395bd6d34eaeea
reginPekin <reginbegin@gmail.com>
2020-01-11 14:58:48 +0300

    index-wrapper-container

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { Suspense, FunctionComponent } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import { Provider, connect } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+
+import { store, persistor } from "./redux/store/store";
+import { Router, View } from "react-navi";
+import { routes } from "./routes";
+import { State } from "./redux/reducers";
+
+import "./style/index.css";
+import styles from "./style/index.module.css";
+
+import { Menu } from "./components/Menu";
+
+interface Props {
+  sortType: string;
+  sortDirection: number;
+}
+
+const AppContainer: FunctionComponent<Props> = ({
+  sortType,
+  sortDirection
+}) => {
+  return (
+    <Router routes={routes} context={{ sortType, sortDirection }}>
+      <main className={styles.app}>
+        <Menu />
+        <Suspense fallback={null}>
+          <section className={styles.main}>
+            <View />
+          </section>
+        </Suspense>
+      </main>
+    </Router>
+  );
+};
+
+const mapStateProps = (state: State) => ({
+  sortType: state.sortTypeReducer.sortType,
+  sortDirection: state.sortTypeReducer.sortDirection
+});
+
+const App = connect(mapStateProps)(AppContainer);
 
 ReactDOM.render(
-  <App name="begin" sername="vagin" />,
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
   document.getElementById("root")
 );
-
-serviceWorker.unregister();
